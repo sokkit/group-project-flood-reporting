09dc6fa742dc0fed9ac2881cea9b9bc7212a8c35
c2061455 <okeefesj@cardiff.ac.uk>
2021-12-28 12:53:52 +0000

    Added paramaterised report overview api tests

--- src/test/java/cf/ac/uk/wrackreport/fullcontainer/ReportOverviews.java
+++ src/test/java/cf/ac/uk/wrackreport/fullcontainer/ReportOverviews.java
@@ -1,6 +1,8 @@
 package cf.ac.uk.wrackreport.fullcontainer;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -30,6 +32,28 @@ public class ReportOverviews {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(12)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"SY18 6RG,1","CG23 9JK, 0", "CF47 8EU, 1"})
+    public void shouldGetNReportOverviewsFromPostcodeAsJSON(String search, String countAsString) throws Exception{
+        Integer count = Integer.valueOf(countAsString);
+        mvc.perform(get("/api/report/exportQuery?postcode="+search+"&localAuthority=&categoryName=&dateFrom=&dateTo=&showRemoved=false").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(count)));
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Flash Flooding,2","Coastal Flooding, 2", "test 0, 0"})
+    public void shouldGetNReportOverviewsFromCategoryNameAsJSON(String search, String countAsString) throws Exception{
+        Integer count = Integer.valueOf(countAsString);
+        mvc.perform(get("/api/report/exportQuery?postcode=&localAuthority=&categoryName="+search+"&dateFrom=&dateTo=&showRemoved=false").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(count)));
+
     }
 
 }
